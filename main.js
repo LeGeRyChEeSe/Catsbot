f39e64901734fcb673a22a11c908d7bbd95450e7
LeGeRyChEeSe <kilian.douarinou41@gmail.com>
2020-05-01 13:24:46 +0200

    Ajout fonctionnalités

---
+++ main.js
@@ -0,0 +1,14 @@
+const { Client } = require('discord.js');
+const client = new Client({ disableMentions: "everyone" });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if(msg.content === 'coucou catsbot') msg.channel.send(`Coucou c'est moi, ${client.user.username} !`);
+  if (msg.content === 'everyone') msg.channel.send("@everyone", { disableMentions: "none" });
+  if (msg.content === 'noteveryone') msg.channel.send("@everyone, but not");
+});
+
+client.login('NzA1NzA0MTMzODc3MDM5MTI0.Xqv36A.r8MZfgXEPPNyALrR3cHg1C0wSQo');
