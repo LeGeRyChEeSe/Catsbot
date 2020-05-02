fc1522082c16b32bb272105ee20d2d03af953e48
LeGeRyChEeSe <kilian.douarinou41@gmail.com>
2020-05-02 14:30:03 +0200

    Alléger le projet

--- commands/repeat.js
+++
@@ -1,6 +0,0 @@
-// Commande pour que le bot répète ce qu'on écrit
-
-module.exports = (client, message, args) => {
-  message.delete();
-  message.channel.send(args.join(" "));
-};
--- events/guildMemberRemove.js
+++
@@ -1,9 +0,0 @@
-// Fonction permettant de notifier le départ d'un membre du serveur
-
-module.exports = (client, member) => {
-  member.send(
-    "J'espère que tu as passé un bon moment avec nous au moins... Sniff :sob:"
-  );
-  const channel = client.channels.cache.find(r => r.name === client.BIENVENUE);
-  channel.send(`Bye bye ${member}, j'espère que tu seras heureux dans ta nouvelle vie :slight_smile:`);
-};
