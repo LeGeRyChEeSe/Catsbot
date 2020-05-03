924eb836e1277d1dc350b07361d6629a6d06b399
Glitch (catsbot) <none>
2020-05-03 05:21:00 +0000

    Bot Musique OP

--- commands/help.js
+++ commands/help.js
@@ -1,11 +1,17 @@
+const { MessageEmbed } = require("discord.js");
+
 module.exports = {
   name: "help",
   description:
     "Renvoie la liste de toutes les commandes disponibles sur le bot",
   execute(msg, args, client) {
+    const tableau = [];
     for (const cmd of client.commands) {
       msg.delete();
-      msg.channel.send(`***c?${cmd[0]}*** : *${cmd[1].description}*`);
+      tableau.push(`***c?${cmd[0]}*** : *${cmd[1].description}*`);
     }
+    
+    msg.channel.send(`${tableau.join("\n\n").concat(" :\n\t")}`);
+
   },
 };
--- commands/stop.js
+++ commands/stop.js
@@ -1,14 +1,13 @@
 module.exports = {
-  name: "stop",
-  description: "Permet d'arrêter la musique.",
-  execute(msg, args, client) {
-    if (!msg.member.voice.channel)
-      return msg.channel.send("Connectez vous à un salon vocal !");
-    if (!msg.guild.me.voice.channel)
-      return msg.channel.send("Je ne suis pas connecté à un salon vocal !");
-    if (msg.guild.me.voice.channelID !== msg.member.voice.channelID)
-      return msg.channel.send("Vous n'êtes pas dans le même salon !");
-    msg.guild.me.voice.channel.leave();
-    msg.delete();
-  },
+	name: 'stop',
+	description: 'Permet de stopper la musique.',
+	cooldown: 5,
+	execute(message) {
+		const { channel } = message.member.voice;
+		if (!channel) return message.channel.send('Vous devez être dans le canal pour jouer de la musique !');
+		const serverQueue = message.client.queue.get(message.guild.id);
+		if (!serverQueue) return message.channel.send('Aucune musique en cours.');
+		serverQueue.songs = [];
+		serverQueue.connection.dispatcher.end('La musique a été coupée !');
+	}
 };
