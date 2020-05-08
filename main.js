6208ad3743e66ff4f9ddf39568ea871affa84c7c
LeGeRyChEeSe <kilian.douarinou41@gmail.com>
2020-05-08 22:14:13 +0200

    Changements

--- main.js
+++ main.js
@@ -4,14 +4,14 @@ const MusicClient = require("./assets/struct/Client");
 const client = new MusicClient({
   token: process.env.TOKEN,
   prefix: process.env.PREFIX,
-  bienvenue: process.env.BIENVENUE
+  bienvenue: process.env.BIENVENUE,
 });
 client.files = new Collection();
 let nouveau_membre = "";
 
 const loadCommands = (dir = "./commands/") => {
-  readdirSync(dir).forEach(dirs => {
-    const commands = readdirSync(`${dir}/${dirs}/`).filter(files =>
+  readdirSync(dir).forEach((dirs) => {
+    const commands = readdirSync(`${dir}/${dirs}/`).filter((files) =>
       files.endsWith(".js")
     );
 
@@ -23,8 +23,8 @@ const loadCommands = (dir = "./commands/") => {
 };
 
 const loadFiles = (dir = "./assets/downloads/hacks/") => {
-  readdirSync(dir).forEach(dirfiles => {
-    const commands = readdirSync(`${dir}/`).filter(files =>
+  readdirSync(dir).forEach(() => {
+    const commands = readdirSync(`${dir}/`).filter((files) =>
       files.endsWith(".zip")
     );
 
@@ -38,11 +38,13 @@ const loadFiles = (dir = "./assets/downloads/hacks/") => {
 
 function loadMessages(dir = "./assets/struct/") {
   let random = 0;
-  const message_onadd = readdirSync(dir).filter(file => file.endsWith(".json"));
+  const message_onadd = readdirSync(dir).filter((file) =>
+    file.endsWith(".json")
+  );
   readFile(`${dir}/${message_onadd}`, (error, message_onadd) => {
     const messages = JSON.parse(message_onadd);
 
-    for (const message in messages) {
+    for (const {} in messages) {
       random++;
     }
     const message_alea = Math.floor(Math.random() * random) + 1;
@@ -59,7 +61,7 @@ function loadMessages(dir = "./assets/struct/") {
 loadCommands();
 loadFiles();
 
-client.on("message", async msg => {
+client.on("message", async (msg) => {
   // Fonction permettant d'exécuter des commandes via le bot
   // La syntaxe d'une commande est : c?<commande> <argument>
   // Par exemple je veux m'ajouter le rôle test : c?role test
@@ -80,9 +82,9 @@ client.on("message", async msg => {
   client.commands.get(cmd).run(msg, args, client);
 });
 
-client.on("guildMemberAdd", member => {
+client.on("guildMemberAdd", (member) => {
   // Fonction permettant de notifier l'arrivée d'un membre sur le serveur
-  
+
   loadMessages();
 
   member.send(`Hey ${member.displayName}, bienvenue sur World War Of Cats :tada::smirk_cat: ! 
@@ -95,19 +97,19 @@ Hey ${member.displayName}, welcome to World War Of Cats :tada::smirk_cat: !
 For security reasons, thanks to write here your GTA nickname ty!
 **:point_right:  Without answers from you within 48 hours, we'll be forced to expel you from our server, thank you in advance for your understanding.**`);
   const channel = client.channels.cache.find(
-    r => r.name === client.config.bienvenue
+    (r) => r.name === client.config.bienvenue
   );
   channel.send(nouveau_membre);
 });
 
-client.on("guildMemberRemove", member => {
+client.on("guildMemberRemove", (member) => {
   // Fonction permettant de notifier le départ d'un membre du serveur
 
   member.send(
     "J'espère que tu as passé un bon moment avec nous au moins... Sniff :sob:"
   );
   const channel = client.channels.cache.find(
-    r => r.name === client.config.bienvenue
+    (r) => r.name === client.config.bienvenue
   );
   channel.send(
     `Bye bye ${member.displayName}, j'espère que tu seras heureux dans ta nouvelle vie :slight_smile:`
