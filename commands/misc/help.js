3f3a373d26d4bd50e95670e186a22f326cb15630
Glitch (catsbot) <none>
2020-06-04 04:45:26 +0000

    Ajout ?server

--- commands/misc/help.js
+++ commands/misc/help.js
@@ -16,11 +16,18 @@ module.exports.run = (client, msg, args) => {
           args[0] === help.help.name ||
           args[0] === `${prefix}${help.help.name}`
         ) {
-          embed.setTitle(info.title);
+          embed.setTitle(info.title.replace(/{guildName}/g, msg.guild.name));
           embed.setColor("RANDOM");
-          embed.setDescription(info.description);
+          embed.setDescription(
+            info.description.replace(/{guildName}/g, msg.guild.name)
+          );
           embed.setThumbnail(client.user.displayAvatarURL());
-          embed.addField("Informations supplémentaires :", info.help.toString().replace(/{prefix}/g, prefix));
+          embed.addField(
+            "Informations supplémentaires :",
+            info.help
+              .replace(/{prefix}/g, prefix)
+              .replace(/{guildName}/g, msg.guild.name)
+          );
           embed.addField("Syntaxe :", `\`${prefix}${info.syntaxe}\``);
           embed.setTimestamp();
           embed.setFooter(client.user.username);
@@ -28,11 +35,13 @@ module.exports.run = (client, msg, args) => {
           embed.setTitle(Name);
           embed.setColor("RANDOM");
           embed.setThumbnail(client.user.displayAvatarURL());
-          Commandes.forEach(name => tabCommandes.push(`\`${prefix}${name.help.name}\``));
+          Commandes.forEach(name =>
+            tabCommandes.push(`\`${prefix}${name.help.name}\``)
+          );
           embed.setDescription(tabCommandes.join(", "));
           embed.setFooter(client.user.username);
           embed.setTimestamp();
-        } else return
+        } else return;
       });
     });
     return msg.channel.send(embed);
@@ -50,7 +59,6 @@ module.exports.run = (client, msg, args) => {
 
   // Aide sur l'ensemble des commandes
   client.commands.each((Commandes, Name) => {
-
     let tabCommandes = new Array();
 
     Commandes.forEach(commande => {
